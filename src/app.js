d777ae7eee9fda5835170ceddafbba10b918adfb
Leonardo Nava (L3oNav) <L3oNav@outlook.com>
2021-01-04 16:26:15 -0800

    course React completed

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './assets/styles/app.scss'
-
+import Header from './components/Header'
 const App = () => {
 	return (
-		<h1>Hola Leonardo</h1>	
+		<Header/>
 	)
 }
 
